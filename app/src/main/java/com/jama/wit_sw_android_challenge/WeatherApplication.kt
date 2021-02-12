420c58f4ff2ad68218e157ac648530a23be57027
Jama Mohamed <jama3137@gmail.com>
2021-02-12 11:02:04 +0300

    [Started] Setup the application class

---
+++ app/src/main/java/com/jama/wit_sw_android_challenge/WeatherApplication.kt
@@ -0,0 +1,10 @@
+package com.jama.wit_sw_android_challenge
+
+import android.app.Application
+import android.util.Log
+
+class WeatherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
