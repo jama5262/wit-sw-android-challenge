215f0746f5dd5efee84d0a4715c0ab7f644c7d9e
Jama Mohamed <jama3137@gmail.com>
2021-02-12 08:08:24 +0300

    [Finished] Added the usecases

---
+++ domain/src/main/java/com/jama/domain/usecases/GetWeatherUseCase.kt
@@ -0,0 +1,14 @@
+package com.jama.domain.usecases
+
+import com.jama.domain.models.WeatherResult
+import com.jama.domain.repositories.WeatherRepository
+
+class GetWeatherUseCase (
+    private val weatherRepository: WeatherRepository
+): BaseUseCase<String, WeatherResult> {
+
+    override suspend fun invoke(param: String): WeatherResult {
+        return weatherRepository.getWeather()
+    }
+
+}
